cbb0df10f1b4249dd337e57ccce525a003952734
KrzysKolo <krzysztofa.kolodziejczak@gmail.com>
2018-11-06 22:57:45 +0100

    poprawa_06.11

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.on('request', function (request, response) {
             response.end();
     } else {
             
-        fs.readFile('./index.html', function(err, html) {
-        response.writeHead(404, {"Content-Type": "text/html; charset=utf-8"});
+        fs.readFile('./obraz.jpg', function(err, html) {
+        response.writeHead(404, {"Content-Type": "jpg; charset=utf-8"});
         response.write(html);
         response.end();
     });
